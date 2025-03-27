7804147e37e4929f3d0becf9c28656cbf8049ab3
ssher1001 <shersumit1001@gmail.com>
2025-03-27 13:57:30 +0530

    Initial message

---
+++ src/main/java/com/sander/fresco/service/SanderFrescoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sander.fresco.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanderFrescoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanderFrescoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sander/fresco/service/SanderFrescoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sander.fresco.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanderFrescoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
