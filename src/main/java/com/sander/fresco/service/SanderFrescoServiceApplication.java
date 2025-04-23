0f4637feff42f0ca260887c498668079782c2fd8
ssher1001 <shersumit1001@gmail.com>
2025-04-23 23:12:33 +0530

    Update SanderFrescoServiceApplication.java

--- src/main/java/com/sander/fresco/service/SanderFrescoServiceApplication.java
+++ src/main/java/com/sander/fresco/service/SanderFrescoServiceApplication.java
@@ -3,8 +3,12 @@ package com.sander.fresco.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
 
 @SpringBootApplication
+@RestController
 public class SanderFrescoServiceApplication {
 
 	public static void main(String[] args) {
